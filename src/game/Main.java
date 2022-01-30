b671b5f778ceb03dfb07f94dbe2aa4471af0cde9
Kirill Tapkov <izhjup@mail.ru>
2022-01-30 12:01:04 +0400

    main logic class is written

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,6 +3,25 @@ package game;
 public class Main {
 
     public static void main (String[] args) {
+        int numberOfMatchesLost=20;
 
+        Console console=new Console();
+        ComputerLogic logic=new ComputerLogic();
+        console.printHello();
+        logic.firstMove();
+        numberOfMatchesLost-= logic.getMatchesTook();
+        console.printComputerTakeMatches(logic.getMatchesTook(),numberOfMatchesLost);
+        while (numberOfMatchesLost>0) {
+            int numberOfMatchesTook=console.readNumberOfMatches();
+            numberOfMatchesLost-=numberOfMatchesTook;
+            console.printUserTookMatches(numberOfMatchesTook,numberOfMatchesLost);
+            if (numberOfMatchesLost==0) {
+
+                break;
+            }
+            logic.takeMatches(numberOfMatchesTook);
+            numberOfMatchesLost-=logic.getMatchesTook();
+            console.printComputerTakeMatches(logic.getMatchesTook(),numberOfMatchesLost);
+        }
     }
 }
